15511e1da76731e5b3913ba1c44ff778523b7f00
Momoruol <90159971+Momoruol@users.noreply.github.com>
2024-10-31 22:26:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function sobre_nos() {
+  window.location.href = 'sobre.html'
+}
+
+function whats() {
+  window.location.href =
+    'https://api.whatsapp.com/send?phone=5554999891819&text=Gostaria%20de%20solicitar%20um%20orçamento'
+}
+
+function contact() {
+  window.location.href = 'contato.html'
+}
